71c796ec2b170161328b0d4626eae6a0b1493a4d
zoraiz <hzoraiz8@gmail.com>
2020-10-26 02:34:38 +0500

    updated server-2

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 let port = process.env.PORT || 3000;
 
+let online = 0;
+
 io.on('connection', (socket) => {
     console.log('User Connected');
     let connections = io.sockets.clients().server.engine.clientsCount;
+    online += 1;
     socket.on('chatmessage', (msg) => {
         if(msg != '') {
             console.log(msg);
@@ -19,7 +22,15 @@ io.on('connection', (socket) => {
             io.emit('device', device)
         }
     });
-    io.emit('connections', connections)
+
+    socket.on('disconnect', () => {
+        console.log('dissconnected');
+        online -= 1;
+        io.emit('connections', online)
+    });
+
+    io.emit('connections', online)
 });
+
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
