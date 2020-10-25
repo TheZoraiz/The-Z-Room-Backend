a2cf410bf0f77ed18ec6c18457deb4072dab10c3
zoraiz <hzoraiz8@gmail.com>
2020-10-25 22:04:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+let port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('User Connected');
+    socket.on('chatmessage', (msg) => {
+        if(msg != '') {
+            console.log(msg);
+            io.emit('chatmessage', msg)
+        }
+    });
+    socket.on('device', (device) => {
+        if(device != '') {
+            console.log(device);
+            io.emit('device', device)
+        }
+    });
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
