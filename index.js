b1d035b922e9486c36127ebf1f8c5ee5ae458aac
zoraiz <hzoraiz8@gmail.com>
2020-10-30 23:31:34 +0500

    fixed error

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ let port = process.env.PORT || 3000;
 
 let online = 0;
 
-let texts = []
-let placeholder = [
-    { name: 'Zoraiz', message: 'Hello there wanderer. I see the wheels of fate have carved your path to arive here' }
+let texts = [
+    { name: 'Zoraiz', message: 'Hello there wanderer. I see the wheels of fate have carved your path to arive here' },
     { name: 'Zoraiz', message: 'I humbly thank you for visiting my precious domain' },
     { name: 'Zoraiz', message: 'Though once this place was lively and undead, it has decayed to this state of unsightly existence' },
-    { name: 'Shafay', message: 'Regardless, please remember, I existed' },
-    { name: 'Shafay', message: 'And I continue to exist' },
-    { name: 'Shafay', message: 'Though most really don\'t care ._.' },
+    { name: 'Zoraiz', message: 'Regardless, please remember, I existed' },
+    { name: 'Zoraiz', message: 'And I continue to exist' },
+    { name: 'Zoraiz', message: 'Though most really don\'t care ._.' },
 ]
-
 io.on('connection', (socket) => {
     console.log('User Connected');
     let connections = socket.client.conn.server.clientsCount;
@@ -30,12 +28,8 @@ io.on('connection', (socket) => {
                 texts = texts.splice(200, 999);
             }
 
-            if(texts.length == 0) {
-                io.emit('chatmessage', placeholder)
-            } else { 
-                console.log(texts);
-                io.emit('chatmessage', texts)
-            }
+            io.emit('chatmessage', texts)
+            
         }
     });
     socket.on('disconnect', () => {
