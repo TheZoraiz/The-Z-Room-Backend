379bed81bc9c3c0877fb3273728919c2da0eadde
Zoraiz <hzoraiz8@gmail.com>
2021-03-02 00:32:14 +0500

    Added chat backup

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const fs = require('fs');
+
 let port = process.env.PORT || 3000;
-// change
+
 let online = 0;
 
 let texts = [
-    { name: 'Zoraiz', message: 'Hello there wanderer. I see the wheels of fate have carved your path to arrive here' },
-    { name: 'Zoraiz', message: 'I humbly thank you for visiting my precious domain' },
-    { name: 'Zoraiz', message: 'Though once this place was lively and undead, it has decayed to this state of unsightly existence' },
-    { name: 'Zoraiz', message: 'Regardless, please remember, I existed' },
-    { name: 'Zoraiz', message: 'And I continue to exist' },
-    { name: 'Zoraiz', message: 'Though most really don\'t care ._.' },
+    
 ]
+
+if(!fs.existsSync('./backup.txt'))
+    fs.writeFileSync('./backup.txt', JSON.stringify(texts))
+else
+    texts = [...JSON.parse(fs.readFileSync('./backup.txt', {encoding:'utf8', flag:'r'}))]
+
 io.on('connection', (socket) => {
     console.log('User Connected');
     let connections = socket.client.conn.server.clientsCount;
@@ -23,7 +26,8 @@ io.on('connection', (socket) => {
         if(msg.message != '') {
             
             texts.push(msg)
-            // Reduce texts on server if they reach 1000
+            fs.writeFileSync('./backup.txt', JSON.stringify(texts))
+
             if(texts.length == 1000) {
                 texts = texts.splice(200, 999);
             }
@@ -44,12 +48,3 @@ io.on('connection', (socket) => {
 
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
-
-var iterator = 0;
-setInterval(() => {
-    console.log(++iterator);
-    if(iterator == 30)
-        iterator = 0;
-}, 5000);
-
-//commento
